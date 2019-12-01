b459358604296afc0c0dca831856f6bbf77e857c
Dor Shaar <dorsr@mta.ac.il>
2019-12-01 22:51:14 +0200

    Add Mediator Pattern

---
+++ DesignPatterns/MediatorPattern/IColleague.cs
@@ -0,0 +1,9 @@
+﻿namespace MediatorPattern
+{
+    public interface IColleague
+    {
+        string Name { get; }
+        void ReceiveRegisterNotification(object sender, ColleagueArgs colleagueArgs);
+        void ReceiveUnRegisterNotification(object sender, ColleagueArgs colleagueArgs);
+    }
+}
---
+++ DesignPatterns/MediatorPattern/IMediator.cs
@@ -0,0 +1,8 @@
+﻿namespace MediatorPattern
+{
+    public interface IMediator
+    {
+        void Register(IColleague colleague);
+        void Unregister(IColleague colleague);
+    }
+}
